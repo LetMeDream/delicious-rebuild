039bacb6e34c029729817c73c04c1fdce85f6201
Raul <raulalfonzo66@gmail.com>
2024-03-11 00:56:38 -0400

    feat (component): create BurguerMenu component that handle navigation

---
+++ components/Search/BurguerMenu.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Separator } from '../ui/separator';
+
+const BurguerMenu = () => {
+
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'Home', href: '/', icon: 'HomeIcon' },
+  {
+    name: 'Italian',
+    href: '/italian',
+    icon: 'DocumentDuplicateIcon',
+  },
+  { name: 'American', href: '/american', icon: 'UserGroupIcon' },
+  { name: 'Thai', href: '/thai', icon: 'UserGroupIcon' },
+  { name: 'Japanese', href: '/japanese', icon: 'UserGroupIcon' },
+];
+
+  return (
+    <nav className='  items-center m-4 cursor-pointer bg-[#d0b5a6] group rounded-t-xl overflow-hidden p-2 mb-6'>
+      <ul className='flex'>
+
+        {links.map((link, i) => {
+          const isLastItem = (i === links.length - 1)
+          return (
+
+            <div
+              key={link.name}
+              className='flex'
+            >
+              <li
+                className='hover:text-white p-2 px-4'
+                >
+                {link.name}
+              </li>
+              {
+                !isLastItem
+                ? (
+                  <Separator className='bg-[#fff7fc]' orientation='vertical'/>
+                ) : null
+              }
+            </div>
+          )
+        })}
+      </ul>
+    </nav>
+  )
+}
+
+export default BurguerMenu
