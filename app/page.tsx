7368e3900cc26d394372972e099ada95a3bb8dd0
Raul <raulalfonzo66@gmail.com>
2024-05-13 23:06:24 -0400

    feat (Search functionality): import Results component in the App root 'index' Page. Show it only when there is a search in our 'searchParams'

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,58 @@
-import Popular from "../components/Popular"
-import Search from "@/components/Search/Search";
+import Popular from "../components/Popular";
 import Veggie from "@/components/Veggie";
+import Results from "@/components/Results";
 
-export default async function Home() {
+
+export default async function Home(
+  {
+    searchParams
+  } :
+  {
+    searchParams?: {
+      search: string
+    }
+  }
+) {
+  await new Promise((res) => { setTimeout(res, 1000) })
   /* Popular */
   const popular = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.API_KEY}&number=8`);
   const popularData = await popular.json()
   const popularRecipes = popularData.recipes
 
   /* Veggie */
-  const veggie = await fetch(`https://api.spoonacular.com/recipes/random?number=10&tags=vegetarian&apiKey=${process.env.API_KEY}`)
+  const veggie = await fetch(`https://api.spoonacular.com/recipes/random?number=8&tags=vegetarian&apiKey=${process.env.API_KEY}`)
   const veggieData = await veggie.json()
   const veggieRecipes = veggieData.recipes
 
-  return (
-    <main className="flex min-h-screen flex-col items-center p-12">
-      <Search />
-  
-      <Veggie 
-        veggieRecipes={veggieRecipes}
-      />
+  /* Handling searching functionality by just performing
+   * a simple 'fetch' using the special 'searchParams' prop. 
+   * */
+  const searchResult = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${searchParams?.search}&apiKey=${process.env.API_KEY}&number=8`)
+  const resultsData = await searchResult.json()
+  const resultRecipes = resultsData.results
 
-      <Popular
-        popularRecipes={popularRecipes}
-      />
+  return (
+    <main className=" flex flex-col gap-2 items-center justify-center">
+  
+      {/* If there's no search */}
+      {
+        !searchParams?.search?.length ?
+        (
+          <>
+            <Veggie veggieRecipes={veggieRecipes} />
+            <Popular popularRecipes={popularRecipes} />
+          </>
+        ) : null       
+      }
+
+      {/* Search result */}
+      { 
+        searchParams?.search?.length 
+        ? <Results resultRecipes={resultRecipes} searchTerm={searchParams?.search} /> 
+        : null
+      }
+
+
     </main>
   );
 }
