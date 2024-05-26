9882c97af953b82f979ec866d717311b6ce5f2f3
Raul <raulalfonzo66@gmail.com>
2024-05-26 10:15:05 -0400

    feat (Image as Backgrounds): stop using Next's Image component and, instead, make the images in the AceternityGrid and FoodCarousel components to be background images, with a little effect on hover

--- components/AceternityGrid.tsx
+++ components/AceternityGrid.tsx
@@ -4,6 +4,10 @@ import { AnimatePresence, motion } from "framer-motion"
 import Image from 'next/image'
 import { Card, CardContent, CardTitle } from './ui/card'
 
+interface CustomStyle extends React.CSSProperties {
+	'--image-url'?: string;
+}
+
 const AceternityGrid = (
     {
 			recipes
@@ -14,66 +18,59 @@ const AceternityGrid = (
 ) => {
   let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
-
   return (
-		<div className=" max-w-[80%] mx-auto px-8">
+		<div className=" mx-auto px-8">
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  py-10">
-			{recipes.map((recipe, idx) => (
-				<div
-					key={recipe.id}
-					className="relative group  block p-2 h-full w-full "
-					onMouseEnter={() => setHoveredIndex(idx)}
-					onMouseLeave={() => setHoveredIndex(null)}
-				>
-					<AnimatePresence>
-						{hoveredIndex === idx && (
-							<motion.span
-								className="absolute inset-0 h-full w-full bg-[#fff7fc]/[0.8] block  rounded-3xl"
-								layoutId="hoverBackground" // required for the background to follow
-								initial={{ opacity: 0 }}
-								animate={{
-									opacity: 1,
-									transition: { duration: 0.15 },
-								}}
-								exit={{
-									opacity: 0,
-									transition: { duration: 0.15, delay: 0.2 },
-								}}
-							/>
-						)}
-					</AnimatePresence>
-					<div className=" rounded-2xl h-full w-full p-4 overflow-hidden bg-gradient-to-br from-[#d0b5a6] to-[#d0b5a6]/[0.2] border border-transparent  group-hover:border-[#fff7fc] relative z-50">
-						<div className="relative z-50">
-							{/* <div className="p-4">
-								<h4 className="text-zinc-100 font-bold tracking-wide mt-4">
-									{recipe.title}
-								</h4>
-								<Image
-										className='rounded-md'
-										src={recipe.image}
-										width={720}
-										height={720}
-										alt={recipe.title}
-									/>
-							</div> */}
-							<Card className='flex flex-col bg-transparent border-none shadow-none'>
-								<CardTitle className=' h-[40px]'>
-									<span className="text-sm text-center font-semibold">{recipe.title}</span>
-								</CardTitle>
-								<CardContent className="flex h-[200px] p-5 py-5">
-									<Image
-										className='rounded-md cursor-pointer'
-										src={recipe.image}
-										width={400}
-										height={400}
-										alt={recipe.title}
-									/>
-								</CardContent>
-							</Card>
+			{recipes.map((recipe, idx) => { 
+				return (
+					<div
+						key={recipe.id}
+						className="relative group  block p-2 h-full w-full "
+						onMouseEnter={() => setHoveredIndex(idx)}
+						onMouseLeave={() => setHoveredIndex(null)}
+					>
+						<AnimatePresence>
+							{hoveredIndex === idx && (
+								<motion.span
+									className="absolute inset-0 h-full w-full bg-[#fff7fc]/[0.8] block  rounded-3xl"
+									layoutId="hoverBackground" // required for the background to follow
+									initial={{ opacity: 0 }}
+									animate={{
+										opacity: 1,
+										transition: { duration: 0.15 },
+									}}
+									exit={{
+										opacity: 0,
+										transition: { duration: 0.15, delay: 0.2 },
+									}}
+								/>
+							)}
+						</AnimatePresence>
+						<div className=" cursor-pointer rounded-2xl h-full w-full p-4 overflow-hidden bg-gradient-to-br from-[#d0b5a6] to-[#d0b5a6]/[0.2] border border-transparent  group-hover:border-[#fff7fc] relative z-50">
+							<div className="relative z-50">
+								<Card className='flex flex-col bg-transparent border-none shadow-none'>
+									<CardTitle className=' h-[40px] group-hover:text-[hsl(21,31%,30%)] transition duration-500 cursor-pointer'>
+										<span className="text-sm text-center font-semibold">{recipe.title}</span>
+									</CardTitle>
+									<CardContent 
+										style={{'--image-url': `url(${recipe?.image})`} as CustomStyle} 
+										className={`flex h-[200px] p-5 py-5 bg-[image:var(--image-url)] bg-cover bg-[100%] hover:bg-[80%] transition-all duration-500 mt-2 cursor-pointer`}>
+										{/* To do later; Reimplement above 'hover' behaviour using Next's optimized Image component below */}
+										{/* <Image
+											className={`rounded-md transition-all duration-500 cursor-pointer ${(hoveredIndex === idx) ? 'transform scale(1.05)' : ''}`}
+											src={recipe.image}
+											width={400}
+											height={400}
+											alt={recipe.title}
+										/> */}
+									</CardContent>
+								</Card>
+							</div>
 						</div>
 					</div>
-				</div>
-			))}
+					)}
+			)
+			}
 			</div>
     </div>
   )
--- components/FoodCarousel.tsx
+++ components/FoodCarousel.tsx
@@ -1,8 +1,12 @@
-import React, { useLayoutEffect } from 'react'
+import React from 'react'
 import { Recipes } from '@/app/lib/definitions'
 import { Carousel, CarouselContent, CarouselNext, CarouselPrevious, CarouselItem } from './ui/carousel'
 import { Card, CardContent, CardTitle } from './ui/card'
 import Image from 'next/image'
+
+interface CustomStyle extends React.CSSProperties {
+	'--image-url'?: string;
+}
 
 const FoodCarousel = ({
   recipes
@@ -22,19 +26,21 @@ const FoodCarousel = ({
           className=''
         >
           {recipes?.map((recipe, index) => (
-            <CarouselItem id='item' key={index} className=" md:basis-2/6 basis-full min-w-0 w-[80%] shrink-0">
-                <Card className='flex flex-col'>
+            <CarouselItem id='item' key={index} className=" md:basis-2/6 basis-full min-w-0 shrink-0">
+                <Card className='flex flex-col bg-[#d0b5a6]'>
                   <CardTitle className=' h-[40px]'>
                     <span className="text-sm text-center font-semibold">{recipe.title}</span>
                   </CardTitle>
-                  <CardContent className="flex h-[280px] p-5 py-10">
-                    <Image
+                  <CardContent 
+                    style={{'--customUrl': `url(${recipe?.image})`} as CustomStyle}
+                    className="flex h-[280px] p-5 py-10 bg-[image:var(--customUrl)] bg-cover bg-[100%] hover:bg-[80%] transition-all duration-500 mt-2 cursor-pointer">
+                    {/* <Image
                       className='rounded-md'
                       src={recipe.image}
                       width={720}
                       height={720}
                       alt={recipe.title}
-                    />
+                    /> */}
                   </CardContent>
                 </Card>
             </CarouselItem>
